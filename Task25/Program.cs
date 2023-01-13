115a802d9fad3c7fdfac81fc6e12d9517f77d50a
EkaterinaBildina <kukresch@gmail.com>
2023-01-13 20:29:05 +0300

    Task25 Program.c commit

---
+++ Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 1)
+{
+    Console.WriteLine("число B введено не корректно (изучить определение 'натуральные числа')!");
+}
+
+else
+{
+    int methodExponent = MethodExponent(numberA, numberB);
+    Console.Write($"Number {numberA} to the power {numberB} = {methodExponent}");
+
+
+    int MethodExponent(int numA, int numB)
+    {
+        int result = 1;
+        for (int i = 1; i <= numB; i++)
+        {
+            result = result * numA;
+        }
+        return result;
+    }
+}
